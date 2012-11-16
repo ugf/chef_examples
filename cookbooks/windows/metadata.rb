f580c570ec9bebf23495cf4e5630519e9131e4df
Isel Fernandez <isel_77@hotmail.com>
2012-11-16 14:09:32 -0500

    adding recipe to execute batch in windows

---
+++ cookbooks/windows/metadata.rb
@@ -0,0 +1,11 @@
+maintainer       "Cloud Infrastructure"
+maintainer_email "csf@ultimatesoftware.com"
+license          "our license"
+description      "Examples of specific Chef features in windows"
+long_description ""
+version          "0.0.1"
+
+supports "windows"
+
+recipe "windows::execute_batch", "executes a batch file"
+
