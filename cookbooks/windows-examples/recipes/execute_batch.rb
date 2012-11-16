c2dbdb05061326972a13aeadafa6b06fb8869327
Isel Fernandez <isel_77@hotmail.com>
2012-11-16 15:07:28 -0500

    fixing incorrect quotes for password

--- cookbooks/windows-examples/recipes/execute_batch.rb
+++ cookbooks/windows-examples/recipes/execute_batch.rb
@@ -1,6 +1,6 @@
 windows_batch "change administrator password" do
   code <<-EOH
     echo changing administrator password
-    net user administrator '@Password02'
+    net user administrator @Password02
   EOH
 end
