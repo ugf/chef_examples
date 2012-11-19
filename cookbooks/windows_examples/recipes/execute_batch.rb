64795a3de82fc54570bdac14ef5f74f9edbc14bb
Isel Fernandez <isel_77@hotmail.com>
2012-11-19 10:07:45 -0500

    trying batch as powershell

--- cookbooks/windows_examples/recipes/execute_batch.rb
+++ cookbooks/windows_examples/recipes/execute_batch.rb
@@ -1,7 +1,15 @@
-windows_batch "change administrator password" do
-  code <<-EOH
-    echo changing administrator password
+#windows_batch "change administrator password" do
+#  code <<-EOH
+#    net user administrator #{node[:windows_examples][:administrator_password]}
+#    wmic UserAccount where "Name='administrator'" set PasswordExpires=false
+#  EOH
+#end
+
+powershell "change administrator password" do
+  script = <<-EOH
+    write-output "changing password to: #{node[:windows_examples][:administrator_password]}"
     net user administrator #{node[:windows_examples][:administrator_password]}
     wmic UserAccount where "Name='administrator'" set PasswordExpires=false
   EOH
+  source(script)
 end
